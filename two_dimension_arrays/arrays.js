c4a9d3384af55f9425d4f7d67e61ee8688242b1a
Suberror27 <abrahamct27@gmail.com>
2024-11-20 15:48:20 -0600

    Modiefied arrays.js and index.html

--- two_dimension_arrays/arrays.js
+++ two_dimension_arrays/arrays.js
@@ -1,14 +1,37 @@
-const grid = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
+// const grid = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
 
 // console.log(grid[0][2]);
 
 // Nested loop structure where i iterates thru rows and the inner loop j iterates thru columns of the 2d array
-for (let i = 0; i < grid.length; i++){
-    for (let j = 0; j < grid[i].length; j++){
-        console.log(`Element at (${i}, ${j}) is: ${grid[i][j]}`);
+// for (let i = 0; i < grid.length; i++){
+//     for (let j = 0; j < grid[i].length; j++){
+//         console.log(`Element at (${i}, ${j}) is: ${grid[i][j]}`);
+//     }
+// }
+
+const theaterSeats = [
+    ["X", "O", "X"],
+    ["O", "X", "O"],
+    ["X", "O", "X"],
+];
+
+function bookSeat(row, col){
+    if (theaterSeats[row][col] === "O"){
+        theaterSeats[row][col] = "X";
+        updateSeatStatus(row, col, "booked");
+        alert(`Seat ${String.fromCharCode(65 + row)}${col + 1} is booked.`);
+    } else {
+        alert(`Seat ${String.fromCharCode(65 + row)}${col + 1} is already take.`)
     }
+};
+
+function updateSeatStatus(row, col, status){
+    const seats = document.getElementsByClassName("seat");
+    const index = row * 3 + col;
+    seats[index].classList.remove("available", "booked");
+    seats[index].classList.add(status);
 }
